607210ac281091d6c6fd2fa50a544457329c8f4c
Mahiyat <mahiyat.tanzim@gmail.com>
2023-11-02 11:04:28 +0600

    Add bar chart draft

--- src/COMPONENTS/ResultStatistics.jsx
+++ src/COMPONENTS/ResultStatistics.jsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from '@mui/material'
 import React from 'react'
+import { Box, Typography } from '@mui/material'
+import { BarChart } from '@mui/x-charts'
 
 export default function ResultStatistics() {
   return (
@@ -30,6 +31,20 @@ export default function ResultStatistics() {
         >
           Result Statistics
         </Typography>
+        <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+          <BarChart
+            xAxis={[
+              { scaleType: "band", data: ["group A", "group B", "group C"] },
+            ]}
+            series={[
+              { data: [4, 3, 5] },
+              { data: [1, 6, 3] },
+              { data: [2, 5, 6] },
+            ]}
+            width={500}
+            height={300}
+          />
+        </Box>
       </Box>
     </Box>
   )
